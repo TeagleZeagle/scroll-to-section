3ee70e0538ed99213c8f4ff55348c46f8ce747df
Kaan Kani İnce <kkince@newmind.ai>
2024-07-10 13:43:43 +0300

    scroll to particular section created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ScrollToSection from "./components/scroll-to-section/scroll-to-section";
+
+function App() {
+    return (
+        <div className="App">
+          {/* Scroll to a Particular Section*/}
+          <ScrollToSection/>
+        </div>
+    );
+}
+
+export default App;
